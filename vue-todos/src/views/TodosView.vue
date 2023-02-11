6284162f15dc360440cddfd9c2836fbee07135d8
Andrey <boombardir20@gmail.com>
2023-02-11 17:44:23 +0500

    TodoItem.vue

--- vue-todos/src/views/TodosView.vue
+++ vue-todos/src/views/TodosView.vue
@@ -1,23 +1,36 @@
 <script setup>
-import {ref} from "vue";
 import {uid} from "uid";
-import TodoCreator from "@/components/TodoCreator.vue";
+import {ref} from "vue";
+import {Icon} from "@iconify/vue";
 
-const todoList = ref([])
+
+import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
+
+const todoList = ref([]);
 const createTodo = (todo) => {
   todoList.value.push({
     id: uid(),
-    todo: todo,
-    isCompleted: null,
+    todo,
+    isCompleted: false,
     isEditing: null,
-  })
-}
+  });
+};
 </script>
 
 <template>
   <main>
     <h1>Create Todo</h1>
-    <TodoCreator @create-todo="createTodo"/>
+    <TodoCreator @create-todo="createTodo">
+      <template #button-content>Create</template>
+    </TodoCreator>
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo"/>
+    </ul>
+    <p v-else class="todos-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face" width="70"/>
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -33,6 +46,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
